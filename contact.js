2aa5e3135baa713313c0c995ff4373482c0511f2
DeadWolftyt <100314496+DeadWolf1tyt@users.noreply.github.com>
2023-02-06 13:32:08 +0300

    Add files via upload

--- contact.js
+++ contact.js
@@ -16,27 +16,32 @@ function openCloseListCity() {
     contactButtonActive.classList.toggle('_active');
     contactButtonUnactive.classList.toggle('_active');
     cityBlockCity.textContent = 'City';
+    cityBlock.classList.remove('_active');
 }
 
 function selectCity0() {
     selectBlockCityList.classList.toggle('_active');
     cityPopup[0].classList.add('_active');
     cityBlockCity.textContent = 'Canandaigua, NY';
+    cityBlock.classList.add('_active');
 }
 function selectCity1() {
     selectBlockCityList.classList.toggle('_active');
     cityPopup[1].classList.add('_active');
     cityBlockCity.textContent = 'New York City';
+    cityBlock.classList.add('_active');
 }
 function selectCity2() {
     selectBlockCityList.classList.toggle('_active');
     cityPopup[2].classList.add('_active');
     cityBlockCity.textContent = 'Yonkers, NY';
+    cityBlock.classList.add('_active');
 }
 function selectCity3() {
     selectBlockCityList.classList.toggle('_active');
     cityPopup[3].classList.add('_active');
     cityBlockCity.textContent = 'Sherrill, NY';
+    cityBlock.classList.add('_active');
 }
 
 selectBlockCityItem[0].addEventListener('click', selectCity0);
